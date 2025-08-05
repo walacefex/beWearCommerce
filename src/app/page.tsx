4da3c9d1241daa00a8781351ed4500b9d152dd6d
walacefex <walacefex01@gmail.com>
2025-08-04 22:49:29 -0300

    chore: add eslint, prettier and shadcn setups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button className="my-1 bg-red-500 px-5">Click me</Button>
     </div>
-  );
+  )
 }
