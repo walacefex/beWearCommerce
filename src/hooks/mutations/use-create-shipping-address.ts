e1250613c1d3dc6ee545ea5ed2169d10f2c7f079
walacefex <walacefex01@gmail.com>
2025-08-15 03:28:40 -0300

    feat: ensure cart shipping address can be selected

---
+++ src/hooks/mutations/use-create-shipping-address.ts
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import { createShippingAddress } from '@/actions/create-shipping-address'
+
+import { getUserAddressesQueryKey } from '../queries/use-user-addresses'
+
+export const getCreateShippingAddressMutationKey = () =>
+  ['create-shipping-address'] as const
+
+export const useCreateShippingAddress = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationKey: getCreateShippingAddressMutationKey(),
+    mutationFn: createShippingAddress,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: getUserAddressesQueryKey(),
+      })
+    },
+  })
+}
