dadb3302c3ecbdd6fa4766773d01bed208640bef
walacefex <walacefex01@gmail.com>
2025-08-15 03:18:06 -0300

    feat: pass initialData as props to useUSerAddrersses

---
+++ src/hooks/queries/use-user-addresses.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { getUserAddresses } from '@/actions/get-user-addresses'
+import { shippingAddressTable } from '@/db/schema'
+
+export const getUserAddressesQueryKey = () => ['user-addresses'] as const
+
+export const useUserAddresses = (params?: {
+  initialData?: (typeof shippingAddressTable.$inferSelect)[]
+}) => {
+  return useQuery({
+    queryKey: getUserAddressesQueryKey(),
+    queryFn: getUserAddresses,
+    initialData: params?.initialData,
+  })
+}
